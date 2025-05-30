8ef7d0619334fc0d23278be2a5ece3e06e5ca3ac
Vu-Sang <Vudinhsang0403@gmail.com>
2025-05-30 16:50:29 +0700

    Add data source configuration for SQL Server in dataSources.xml

--- src/main/java/com/HSF301/pojo/Subject.java
+++ src/main/java/com/HSF301/pojo/Subject.java
@@ -25,39 +25,4 @@ public class Subject {
     @ManyToOne
     @JoinColumn(name = "student_id", referencedColumnName = "id")
     private Student student;
-    public Subject(String nameSubject, String studentName) {
-        this.Name_Subject = nameSubject;
-        this.studentName = studentName;
-    }
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName_Subject() {
-        return Name_Subject;
-    }
-
-    public void setName_Subject(String name_Subject) {
-        Name_Subject = name_Subject;
-    }
-
-    public Student getStudent() {
-        return student;
-    }
-
-    public void setStudent(Student student) {
-        this.student = student;
-    }
-
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
 }
